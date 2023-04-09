ce7f8945110eefd70bcd88072ea616df671ccc66
jeffthechristian <daiiviids@gmail.com>
2023-04-09 14:30:56 +0300

    updated car event

--- Assets/MyScripts/CarFallScript.cs
+++ Assets/MyScripts/CarFallScript.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class CarFallScript : MonoBehaviour
 {
-    public Animator car;
-    private bool shouldFall = false;
-    private bool isFall = false;
+public Animator car;
+public GameObject enemy;
+private bool shouldFall = false;
+private bool isFall = false; 
 
-     void OnTriggerEnter(Collider other)
+void OnTriggerEnter(Collider other)
+{
+    if (other.gameObject.tag == "Player" && !isFall)
     {
-        if (other.gameObject.tag == "Player" && !isFall)
-        {
-            shouldFall = true;
-        }
+        shouldFall = true;
     }
+}
 
-    void Update() {
-        if (shouldFall) {
-             car.SetBool("isNear", true);
-             isFall = true;
-        }
+void Update() {
+    if (shouldFall) {
+         car.SetBool("isNear", true);
+         isFall = true;
+         StartCoroutine(DisappearEnemy());
     }
+}
+
+IEnumerator DisappearEnemy() {
+    yield return new WaitForSeconds(2f);
+    enemy.SetActive(false);
+}
 }
