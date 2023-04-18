030d3e3ba252df0c0ae592ec7780675d107c7753
jeffthechristian <daiiviids@gmail.com>
2023-04-18 12:06:37 +0300

    added damage effect

--- Assets/MyScripts/PlayerHealthScript.cs
+++ Assets/MyScripts/PlayerHealthScript.cs
@@ -5,17 +5,25 @@ using UnityEngine.UI;
 public class PlayerHealthScript : MonoBehaviour {
     public float cooldown = 2f;
     public static float currentHealth;
+    public RawImage damageEffectImage;
+    public AudioClip damageAudio;
+    public GameObject deathAudio;
+    private AudioSource audioSource;
     public GameObject deathText;
     public Text healthText;
     private bool canTakeDamage = true;
 
     public Text coinText;
-    public static int coinCount;
+    public static int coinCount;  
 
     void Start() {
         deathText.SetActive(false);
         healthText.text = "HEALTH: " + currentHealth + "HP";
         currentHealth = DifficultyScript.playerHealth;
+        damageEffectImage.color = Color.clear;
+        damageEffectImage.enabled = true;
+        audioSource = GetComponent<AudioSource>();
+        deathAudio.SetActive(false);
     }
 
     void OnTriggerEnter(Collider other) {
@@ -24,11 +32,14 @@ public class PlayerHealthScript : MonoBehaviour {
             currentHealth--;
             canTakeDamage = false;
             StartCoroutine(Cooldown());
+            StartCoroutine(ShowDamageEffect());
+            audioSource.PlayOneShot(damageAudio);
         }
     }
 
     void Update() {
         if (currentHealth <= 0f) {
+            deathAudio.SetActive(true);
             Time.timeScale = 0f;
             deathText.SetActive(true);
             Cursor.visible = true;
@@ -37,10 +48,24 @@ public class PlayerHealthScript : MonoBehaviour {
         }
         healthText.text = "HEALTH: " + currentHealth + "HP";
         coinText.text = "MONEY: " + coinCount;
+
+        if (Time.timeScale == 0f) {
+            damageEffectImage.enabled = false;
+        } else {
+            damageEffectImage.enabled = true;
+        }
     }
 
     IEnumerator Cooldown() {
         yield return new WaitForSeconds(cooldown);
         canTakeDamage = true;
+    }
+
+    IEnumerator ShowDamageEffect() {
+        if (currentHealth > 0f) {
+            damageEffectImage.color = Color.red;
+            yield return new WaitForSeconds(0.1f);
+            damageEffectImage.color = Color.clear;
+        }
     }
 }
