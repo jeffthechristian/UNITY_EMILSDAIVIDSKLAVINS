2ec409030cac388c0100c6564a44a59d0334e7e4
jeffthechristian <daiiviids@gmail.com>
2023-05-30 15:35:39 +0300

    scary ghost at church :O

---
+++ Assets/MyScripts/ChurchGhostScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChurchGhostScript : MonoBehaviour
+{
+    public GameObject ghost;
+    public GameObject shh;
+    public GameObject whisper;
+    private bool isSpooky;
+    private bool isScary;
+
+    void Start()
+    {
+        ghost.SetActive(false);
+        shh.SetActive(false);
+        whisper.SetActive(true);
+        isSpooky = false;
+        isScary = false;
+    }
+
+    void Update()
+    {
+        if (QuestLogScript.cluesCount == 3 && !isScary)
+        {
+            ghost.SetActive(true);
+            isSpooky = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (isSpooky)
+        {
+            isScary = true;
+            ghost.SetActive(false);
+            shh.SetActive(true);
+            whisper.SetActive(false);
+        }
+    }
+}
